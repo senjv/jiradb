37fa830fa07d715d1aa1c6cc0c526d097477e291
Senen Estuista <senen.estuista@sensis.com.au>
2017-08-18 10:38:02 +0800

    cleanup

--- app/view1/getData.sh
+++ app/view1/getData.sh
@@ -2,6 +2,6 @@
 
 
 
-curl -u w38001:Chrmndr316! https://jira.in.sensis.com.au/rest/api/latest/search?jql=assignee=senen > data.json
+curl -u u:p! https://jira.in.sensis.com.au/rest/api/latest/search?jql=assignee=senen > data.json
 
 #'https://jira.in.sensis.com.au/rest/api/2/search?jql=project %3D \"myAccount (SDF Customer Centre)\" AND fixVersion %3D R13.0.1' > data.json
