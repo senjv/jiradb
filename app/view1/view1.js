41fb9a085de18066254bc58bb865cc5a9881b16f
Senen Estuista <senen.estuista@sensis.com.au>
2017-08-18 10:36:44 +0800

    add node modules + sample graphs

--- app/view1/view1.js
+++ app/view1/view1.js
@@ -14,17 +14,117 @@ angular.module('myApp.view1', ['ngRoute'])
 
   $http.get('view1/data.json').then(function(data) {
     $scope.project = data;
-    console.log($scope.project);
 
     Highcharts.chart('container', {
+      chart: {
+        width: '500'
+      },
+
+      title: {
+        text: 'Velocity'
+      },
+
+      yAxis: {
+        title: {
+          text: 'Points Completed'
+        }
+      },
 
       xAxis: {
-        categories: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun',
-          'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
+        categories: ['Sprint 1', 'Sprint 2', 'Sprint 3', 'Sprint 4', 'Sprint 5', 'Sprint 6',
+          'Sprint 7', 'Sprint 8', 'Sprint 9', 'Sprint 10', 'Sprint 11', 'Sprint 12']
       },
 
       series: [{
-        data: [29.9, 71.5, 106.4, 129.2, 144.0, 176.0, 135.6, 148.5, 216.4, 194.1, 95.6, 54.4]
+        name: 'Salesforce',
+        data: [25, 20, 22, 24, 19, 27, 30, 28, 28, 35, 30, 32]
+      }]
+    });
+
+    //container 2
+    Highcharts.chart('container2', {
+      chart: {
+        width: '500'
+      },
+
+      title: {
+        text: 'Productivity'
+      },
+
+      yAxis: {
+        title: {
+          text: 'Points Completed'
+        }
+      },
+
+      xAxis: {
+        categories: ['August 1', 'August 2', 'August 3', 'August 4', 'August 5', 'August 6',
+          'August 7', 'August 8', 'August 9', 'August 10', 'August 11', 'August 12']
+      },
+
+      series: [{
+        name: 'Salesforce',
+        data: [25, 20, 22, 24, 19, 27, 30, 28, 28, 35, 30, 32]
+      }]
+    });
+
+    //container 3
+    Highcharts.chart('container3', {
+      chart: {
+        width: '500'
+      },
+
+      title: {
+        text: 'Capacity'
+      },
+
+      yAxis: {
+        title: {
+          text: 'Points Remaining'
+        }
+      },
+
+      xAxis: {
+        title: {
+          text: 'Capacity Remaining'
+        },
+        categories: ['August 1', 'August 2', 'August 3', 'August 4', 'August 5', 'August 6',
+          'August 7', 'August 8', 'August 9', 'August 10', 'August 11', 'August 12']
+      },
+
+      series: [{
+        name: 'Salesforce',
+        data: [25, 20, 22, 24, 19, 27, 30, 28, 28, 35, 30, 32]
+      }]
+    });
+
+    //container 4
+    Highcharts.chart('container4', {
+      chart: {
+        width: '500'
+      },
+
+      title: {
+        text: 'Scope Volatility'
+      },
+
+      yAxis: {
+        title: {
+          text: 'Scope at Post Start of Iteration'
+        }
+      },
+
+      xAxis: {
+        title: {
+          text: 'Scope at Start of Iteration'
+        },
+        categories: ['August 1', 'August 2', 'August 3', 'August 4', 'August 5', 'August 6',
+          'August 7', 'August 8', 'August 9', 'August 10', 'August 11', 'August 12']
+      },
+
+      series: [{
+        name: 'Salesforce',
+        data: [25, 20, 22, 24, 19, 27, 30, 28, 28, 35, 30, 32]
       }]
     });
   });
